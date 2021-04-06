3f247160119c9484d2b8fb49caeff868458413f3
Dário Junior <dariojunior04@gmail.com>
2021-04-06 00:53:06 -0300

    Criando seeder para administrador

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@exemplo.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' // password
+        ]);
+         \App\Models\User::factory(5)->create();
     }
 }

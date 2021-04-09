c20ce9747a7c1dbfc163d9921d9e97e8f5e3d000
Lael Ramos <laelrf@gmail.com>
2021-04-09 17:12:38 -0300

    status financeiro

--- resources/views/fluxo.blade.php
+++ resources/views/fluxo.blade.php
@@ -1,8 +1,43 @@
 @extends('layouts.app')
 
 @section('page-title', 'Matrix Admin :: Fluxo')
-@section('title', 'Fluxo')
+@section('title', 'Fluxo De Caixa')
 
 @section('main')
-    Aqui vão as movimentações do caixa
+
+<div class="col-lg-6">
+    <!-- SALDO -->
+    <div class="col-md-6 col-lg-6 col-xlg-3">
+        <div class="card card-hover">
+            <div class="box bg-success text-center">
+                <h1 class="font-light text-white"><i class="mdi mdi-view-dashboard"></i></h1>
+                <h6 class="text-white">Saldo</h6>
+            </div>
+        </div>
+    </div>
+
+    <!-- A RECEBER -->
+    <div class="col-md-6 col-lg-6 col-xlg-3">
+        <div class="card card-hover">
+            <div class="box bg-cyan text-center">
+                <h1 class="font-light text-white"><i class="mdi mdi-view-dashboard"></i></h1>
+                <h6 class="text-white">à Receber</h6>
+            </div>
+        </div>
+    </div>
+
+    <!-- CREDIARIO -->
+    <div class="col-md-6 col-lg-6 col-xlg-3">
+        <div class="card card-hover">
+            <div class="box bg-warning text-center">
+                <h1 class="font-light text-white"><i class="mdi mdi-receipt"></i></h1>
+                <h6 class="text-white">Crediário</h6>
+            </div>
+        </div>
+    </div>
+
+<button type="button" class="btn btn-success text-white"><b> Nova Receita</b></button> 
+<button type="button" class="btn btn-danger text-white"><b> Nova Despesa</b></button>
+</div>
+
 @endsection

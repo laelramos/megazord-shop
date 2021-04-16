3591d014f834fd9bdf56c7bdfafc1a980d483e24
Lael Ramos <laelrf@gmail.com>
2021-04-16 16:18:39 -0300

    pagina estoque

--- resources/views/estoque.blade.php
+++ resources/views/estoque.blade.php
@@ -4,73 +4,62 @@
 @section('title', 'Estoque')
 
 @section('main')
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Estoque</h5>
-            <div class="table-responsive">
-                <table id="zero_config" class="table table-striped table-bordered">
-                    <thead>
-                        <tr>
-                            <th>Foto</th>
-                            <th>Código</th>
-                            <th>Categoria</th>
-                            <th>Produto</th>
-                            <th>Qtd.</th>
-                            <th>Custo</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Foto 1</td>
-                            <td>12121212</td>
-                            <td>Funko</td>
-                            <td>Kakashi #182</td>
-                            <td>2</td>
-                            <td>R$ 85,00</td>
-                        </tr>
-                        <tr>
-                            <td>Foto 2</td>
-                            <td>01010226</td>
-                            <td>Figure</td>
-                            <td>Gaara 01</td>
-                            <td>1</td>
-                            <td>R$ 35,00</td>
-                        </tr>
-                        <tr>
-                            <td>Foto 3</td>
-                            <td>00112233</td>
-                            <td>Camiseta</td>
-                            <td>Raglan branca</td>
-                            <td>17</td>
-                            <td>R$ 11,00</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-
-        </div>
-    </div>
-
-    <script src="../../assets/libs/jquery/dist/jquery.min.js"></script>
-    <!-- Bootstrap tether Core JavaScript -->
-    <script src="../../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- slimscrollbar scrollbar JavaScript -->
-    <script src="../../assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
-    <script src="../../assets/extra-libs/sparkline/sparkline.js"></script>
-    <!--Wave Effects -->
-    <script src="../../dist/js/waves.js"></script>
-    <!--Menu sidebar -->
-    <script src="../../dist/js/sidebarmenu.js"></script>
-    <!--Custom JavaScript -->
-    <script src="../../dist/js/custom.min.js"></script>
-    <!-- this page js -->
-    <script src="../../assets/extra-libs/multicheck/datatable-checkbox-init.js"></script>
-    <script src="../../assets/extra-libs/multicheck/jquery.multicheck.js"></script>
-    <script src="../../assets/extra-libs/DataTables/datatables.min.js"></script>
-    <script>
-        /****************************************
-         *       Basic Table                   *
-         ****************************************/
-        $('#zero_config').DataTable();
-    </script>
+    <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <!-- Column -->
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table product-overview" id="zero_config">
+                                        <thead>
+                                            <tr>
+                                                <th>Foto</th>
+                                                <th>Código</th>
+                                                <th>Categoria</th>
+                                                <th>Descrição</th>
+                                                <th>Quantidade</th>
+                                                <th>Custo</th>
+                                                <th>Edit</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td> <img src="../../assets/images/gallery/kakashiF.jpg" alt="iMac" width="80"></td>
+                                                <td>#85457898</td>
+                                                <td> Funko </td>
+                                                <td>Funko Pop! Kakashi #182</td>
+                                                <td>2</td>
+                                                <td>85,90</td>
+                                                <td><a href="javascript:void(0)" class="text-inverse p-r-10" data-toggle="tooltip" title="Edit"><i class="ti-marker-alt"></i></a> <a href="javascript:void(0)" class="text-inverse" title="Delete" data-toggle="tooltip"><i class="ti-trash"></i></a></td>
+                                            </tr>
+                                            <tr>
+                                                <td> <img src="../../assets/images/gallery/kakashiF.jpg" alt="iMac" width="80"></td>
+                                                <td>#298634129</td>
+                                                <td> Funko </td>
+                                                <td>Funko Pop! Kakashi #182</td>
+                                                <td>2</td>
+                                                <td>85,90</td>
+                                                <td><a href="javascript:void(0)" class="text-inverse p-r-10" data-toggle="tooltip" title="Edit"><i class="ti-marker-alt"></i></a> <a href="javascript:void(0)" class="text-inverse" title="Delete" data-toggle="tooltip"><i class="ti-trash"></i></a></td>
+                                            </tr>
+                                            <tr>
+                                                <td> <img src="../../assets/images/gallery/kakashiF.jpg" alt="iMac" width="80"></td>
+                                                <td>#801923810</td>
+                                                <td> Funko </td>
+                                                <td>Funko Pop! Kakashi #182</td>
+                                                <td>2</td>
+                                                <td>85,90</td>
+                                                <td><a href="javascript:void(0)" class="text-inverse p-r-10" data-toggle="tooltip" title="Edit"><i class="ti-marker-alt"></i></a> <a href="javascript:void(0)" class="text-inverse" title="Delete" data-toggle="tooltip"><i class="ti-trash"></i></a></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                </div>
+                <!-- ============================================================== -->
+                <!-- End PAge Content -->
 @endsection
